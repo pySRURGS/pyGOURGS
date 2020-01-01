a439cdb10fd4d4c84b047d262df372fcb2d3ad9a
sohrabtowfighi <sohrabtowfighi@hotmail.com>
2020-01-01 17:37:41 -0500

    latest

--- setup.py
+++ setup.py
@@ -14,6 +14,10 @@ setuptools.setup(
     url="https://github.com/pySRURGS/pyGOURGS",
     packages=["pyGOURGS"],
     py_modules=["pyGOURGS"],
+    package_data = {
+    'examples': ['*'],
+    '': ['test.py', 'LICENSE.txt', 'README.md']
+    }
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
