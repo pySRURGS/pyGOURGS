8755eda67f5f44a7e84f63a0dfb3199d4786b21f
sohrabtowfighi <sohrabtowfighi@hotmail.com>
2020-02-18 19:53:31 -0500

    added some lines to install script for ease

--- experiments/install.sh
+++ experiments/install.sh
@@ -2,3 +2,6 @@ sudo apt update
 sudo apt install python3
 sudo apt install python3-pip
 sudo apt install git
+git clone https://github.com/pySRURGS/pyGOURGS.git
+cd pyGOURGS 
+pip3 install -r requirements.txt --user 
